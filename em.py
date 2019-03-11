94221083accd19cc91a0eb1b57d3d95d8c6c1201
Lorenzo Vainigli <lorenzovngl@gmail.com>
2019-03-11 17:43:24 +0100

    Finished to implement EM algorithm

--- em.py
+++ em.py
@@ -1,8 +1,8 @@
 import random
 import reducebykey as reduce
 import utils
-import functools as ft
 from math import log
+import numpy as np
 
 answers = 0
 correct_answers = 0
@@ -46,7 +46,7 @@ count_fs = list(map(lambda w: (w, 1), count_fs))
 utils.print_to_file('count_fs.json', count_fs)
 count_fs = list(reduce.reduceByKey(lambda x, y: x + y, count_fs))
 
-print("Initializing parameters...")
+print("Initializing parameters randomly...")
 # Random initialization
 # P(f_j|s_k) = count(f_j, s_k)/count(s_k)
 P_fs = {}
@@ -68,70 +68,118 @@ for ws in count_ws:
             P_s[ws[0][1]] = random.uniform(0, 1)
 utils.print_to_file('P_s.json', P_s)
 
-# P(c_i|s_k) = PROD_{f_j in c_i) (P(f_j|s_k))
 P_cs = []
-for text in corpus:
-    for i, sentence in enumerate(text):
-        temp = {}
-        for word in filter(lambda w: w['sense'] != '', sentence):
-            try:
-                for possible_sense in possible_senses[word['lemma']]:
-                    # print("- Possible sense", possible_sense)
-                    product = 1
-                    for feature_in_sentence in filter(lambda w: w['sense'] != '' and w['lemma'] != word['lemma'], sentence):
-                        # print("--", feature_in_sentence['lemma'])
-                        found = False
-                        for feature_in_senses in P_fs[possible_sense]:
-                            if feature_in_sentence['lemma'] == feature_in_senses:
-                                found = True
-                                # print("---", feature_in_senses, P_fs[possible_sense][feature_in_senses])
-                                product *= P_fs[possible_sense][feature_in_senses]
-                                break
-                        if not found:
-                            product = 0
-                    # print("P(c_" + str(i), "|", possible_sense, ") =", product)
-                    temp[possible_sense] = product
-            except KeyError:
-                print('KeyError: ' + word['lemma'])
-        P_cs.append(temp)
-utils.print_to_file('P_cs.json', P_cs)
-
-# P(c_i) = SUM P(c_i|s_k)P(s_k)
 P_c = []
-for context in P_cs:
-    sum = 0
-    for sense in context:
-        sum += context[sense] * P_s[sense]
-    P_c.append(sum)
-utils.print_to_file('P_c.json', P_c)
-
-# l(C) -> Likelihood of the corpus
-sum_contexts = ft.reduce(lambda x, y: x*y, P_c)
-likelihood = log(sum_contexts)
-print(likelihood)
-
-# E-step
-# h_ik = P(c_i|s_k)/SUM(P(c_i|s_k)
 h_ik = []
-for i, context in enumerate(P_cs):
-    temp = {}
-    for sense in context:
-        temp[sense] = P_cs[i][sense]/P_c[i]
-    h_ik.append(temp)
-utils.print_to_file('h_ik.json', h_ik)
 
-# M-step
-for sense in P_fs:
-    sum = 0
-    for feature in P_fs[sense]:
-        for text in corpus:
-            for i, sentence in enumerate(text):
+
+def compute_likelihood():
+    # P(c_i|s_k) = PROD_{f_j in c_i) (P(f_j|s_k))
+    P_cs.clear()
+    P_c.clear()
+    for text in corpus:
+        for i, sentence in enumerate(text):
+            temp = {}
+            for word in filter(lambda w: w['sense'] != '', sentence):
                 try:
-                    if feature in [x['lemma'] for x in sentence]:
-                        sum += h_ik[i][sense]
+                    for possible_sense in possible_senses[word['lemma']]:
+                        # print("- Possible sense", possible_sense)
+                        product = 1
+                        for feature_in_sentence in filter(lambda w: w['sense'] != '' and w['lemma'] != word['lemma'], sentence):
+                            # print("--", feature_in_sentence['lemma'])
+                            found = False
+                            for feature_in_senses in P_fs[possible_sense]:
+                                if feature_in_sentence['lemma'] == feature_in_senses:
+                                    found = True
+                                    # print("---", feature_in_senses, P_fs[possible_sense][feature_in_senses])
+                                    product *= P_fs[possible_sense][feature_in_senses]
+                                    break
+                            if not found:
+                                product = 0
+                        # print("P(c_" + str(i), "|", possible_sense, ") =", product)
+                        temp[possible_sense] = product
                 except KeyError:
-                    # TODO: Too much keyerrors, probably there is a bug
-                    pass
-                    # print('KeyError:', feature)
-    print(sum)
+                    print('KeyError: ' + word['lemma'])
+            P_cs.append(temp)
+    utils.print_to_file('P_cs.json', P_cs)
 
+    # P(c_i) = SUM P(c_i|s_k)P(s_k)
+    for context in P_cs:
+        sum = 0
+        for sense in context:
+            sum += context[sense] * P_s[sense]
+        P_c.append(sum)
+    utils.print_to_file('P_c.json', P_c)
+
+    # l(C) -> Likelihood of the corpus
+    prod_contexts = np.prod(P_c)
+    likelihood = log(prod_contexts)
+    print("Likelihood:", likelihood)
+    return likelihood
+
+
+def e_step():
+    # E-step
+    # h_ik = P(c_i|s_k)/SUM(P(c_i|s_k)
+    h_ik.clear()
+    print("E-step...")
+    for i, context in enumerate(P_cs):
+        temp = {}
+        for sense in context:
+            temp[sense] = P_cs[i][sense]/P_c[i]
+        h_ik.append(temp)
+    utils.print_to_file('h_ik.json', h_ik)
+
+
+def m_step():
+    # M-step
+    print("M-step...")
+    Z = {}
+    for sense in P_fs:
+        for feature in P_fs[sense]:
+            sum = 0
+            Z[feature] = 0
+            for text in corpus:
+                for i, sentence in enumerate(text):
+                    try:
+                        if feature in [x['lemma'] for x in sentence]:
+                            sum += h_ik[i][sense]
+                    except KeyError:
+                        # TODO: Too much keyerrors, probably there is a bug
+                        pass
+                        # print('KeyError:', feature)
+            # print(feature, sum)
+            Z[feature] += sum
+            P_fs[sense][feature] = sum / Z[feature]
+    utils.print_to_file('P_fs.json', P_fs)
+
+    total = 0
+    for sense in P_fs:
+        for i, context in enumerate(h_ik):
+            try:
+                total += h_ik[i][sense]
+            except KeyError:
+                pass
+
+    for sense in P_fs:
+        sum = 0
+        for i, context in enumerate(h_ik):
+            try:
+                sum += h_ik[i][sense]
+            except KeyError:
+                pass
+        P_s[sense] = sum / total
+    utils.print_to_file('P_s.json', P_s)
+
+
+# Core algorithm
+prev_likelihood = compute_likelihood()
+e_step()
+m_step()
+likelihood = compute_likelihood()
+
+while likelihood > prev_likelihood:
+    e_step()
+    m_step()
+    prev_likelihood = likelihood
+    likelihood = compute_likelihood()
